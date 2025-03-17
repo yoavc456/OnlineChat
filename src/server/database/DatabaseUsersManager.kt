fc71a35621d8560765acdd317b37ed518461f128
Yoav <yoavcohenshapira@gmail.com>
2025-03-17 17:09:47 +0200

    tb-test

--- src/server/database/DatabaseUsersManager.kt
+++ src/server/database/DatabaseUsersManager.kt
@@ -4,4 +4,6 @@ interface DatabaseUsersManager {
     suspend fun doesUserExist(username: String, password: String): Boolean
     suspend fun doesUsernameExist(username: String): Boolean
     suspend fun createUser(username: String, password: String): Boolean
+
+    //aaabbb
 }
